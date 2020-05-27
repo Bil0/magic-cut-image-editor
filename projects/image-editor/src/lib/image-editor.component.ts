a74968d2f358895b6fb7ba7432ea7c9755426b02
Billel LASLEDJ <b.lasledj@gmail.com>
2020-05-27 12:26:33 +0200

    chore(global): init projet

---
+++ projects/image-editor/src/lib/image-editor.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-image-editor',
+  template: `
+    <p>
+      image-editor works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ImageEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/image-editor/src/lib/image-editor.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { ImageEditorComponent } from './image-editor.component';
+
+
+
+@NgModule({
+  declarations: [ImageEditorComponent],
+  imports: [
+  ],
+  exports: [ImageEditorComponent]
+})
+export class ImageEditorModule { }
